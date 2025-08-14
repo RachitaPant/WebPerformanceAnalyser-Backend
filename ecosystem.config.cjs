fd1c4568f0bc277cf5820661d3860aaf641190f3
Rachita Pant <rachitap89@gmail.com>
2025-08-14 23:15:55 +0530

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,6 @@ module.exports = {
       watch: false,
       instances: 1,
       autorestart: true,
-      max_memory_restart: "2G",
       error_file: "./logs/error.log",
       out_file: "./logs/out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
